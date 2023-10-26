eee89846a69cab7a675ab2d324ce55006f33532e
ayberktmn <ayberktemin@gmail.com>
2023-10-26 18:09:31 +0300

    countrys added

--- app/src/main/java/com/ayberk/composeapp/anasayfa.kt
+++ app/src/main/java/com/ayberk/composeapp/anasayfa.kt
@@ -1,20 +1,28 @@
 package com.ayberk.composeapp
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -74,34 +83,57 @@ fun CountryListView(countrys: List<CountryItem>,navController: NavController) {
     LazyColumn(contentPadding = PaddingValues(5.dp)) {
         items(countrys) { country ->
             CoutryRow(navController = navController, country = country)
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(5.dp))
         }
     }
 }
 
 @Composable
 fun CoutryRow(navController: NavController, country: CountryItem) {
-    Box() {}
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .background(color = Color.Black)
-        .clickable {
-            //  navController.navigate("crypto_detail_screen/${crypto.currency}/${crypto.price}")
-        }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(1.dp, Color.Gray) // Kenarlıkları ekleyin
     ) {
-        Text(text = country.UlkeAdi,
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.padding(2.dp),
-            fontWeight = FontWeight.Bold,
-            color = Color.Yellow
-        )
-        Text(text = country.UlkeAdiEn,
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(2.dp),
-            color = Color.White
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Black)
+                .clickable {
+                    //  navController.navigate("crypto_detail_screen/${crypto.currency}/${crypto.price}")
+                },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.country), // Resminizi drawable'dan alın
+                contentDescription = null,
+                modifier = Modifier
+                    .size(50.dp)
+                    .padding(8.dp)
+            )
+            Column {
+                Text(
+                    text = country.UlkeAdi,
+                    style = MaterialTheme.typography.headlineLarge,
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .align(Alignment.Start), // Yazıyı sola hizala
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Yellow,
+                )
+                Text(
+                    text = country.UlkeAdiEn,
+                    style = MaterialTheme.typography.labelLarge,
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .align(Alignment.Start), // Yazıyı sola hizala
+                    color = Color.White
+                )
+            }
+        }
     }
 }
+
 
 @Composable
 fun RetryView(
